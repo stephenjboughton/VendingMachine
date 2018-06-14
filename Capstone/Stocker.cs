3b112e421f536f7a2cafc15d97498c91ea481441
Andrew McCarthy <andrewmcc1120@gmail.com>
2018-06-14 14:33:23 -0400

    Main UI is operational

--- Capstone/Stocker.cs
+++ Capstone/Stocker.cs
@@ -11,7 +11,26 @@ namespace Capstone
 	public class Stocker
 	{
 		string path = Path.Combine(Environment.CurrentDirectory, "vendingmachine.txt");
-		List<PurchasableItem> quiz = new List<PurchasableItem>();
 
+		private static List<PurchasableItem> ReturnListOfStock(string path)
+		{
+			List<PurchasableItem> itemsToStock = new List<PurchasableItem>();
+			try
+			{
+				using (StreamReader sr = new StreamReader(path))
+				{
+					while (!sr.EndOfStream)
+					{
+						string[] line = sr.ReadLine().Split('|');
+						itemsToStock.Add(new PurchasableItem(line[1], decimal.Parse(line[2])));
+					}
+				}
+			}
+			catch (IOException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			return itemsToStock;
+		}
 	}
 }
