3b0d8571511b35bb91621702a820a6e72ef07639
Stephen Boughton <stephenjboughton@gmail.com>
2018-06-19 15:38:01 -0400

    trial unit tests

---
+++ Capstone.Tests/HasRequiredBalanceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Capstone.Tests
+{
+	[TestClass]
+	public class HasRequiredBalanceTest
+	{
+		[DataTestMethod]
+		[DataRow("A1", false)]
+		[DataRow("A3", false)]
+		[DataRow("C3", true)]
+		[DataRow("D2", true)]
+		public void CheckToSeeIfHasRequiredBalance(string selection, bool expectedResult)
+		{
+			string path = Path.Combine(Environment.CurrentDirectory, "vendingmachine.txt");
+			Stocker stocker = new Stocker();
+			VendingMachine vendingMachine = new VendingMachine(stocker.ReturnStock(path));
+
+			vendingMachine.FeedMoney(2);
+			bool result = vendingMachine.HasRequiredBalance(selection);
+
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
---
+++ Capstone.Tests/ProductInStockTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Capstone.Tests
+{
+	[TestClass]
+	public class ProductInStockTest
+	{
+		[DataTestMethod]
+		[DataRow("A1", true)]
+		[DataRow("A3", false)]
+		[DataRow("D2", true)]
+		public void CheckToSeeIfProductInStockMethodWorks(string selection, bool expectedResult)
+		{
+			string path = Path.Combine(Environment.CurrentDirectory, "vendingmachine.txt");
+			Stocker stocker = new Stocker();
+			VendingMachine vendingMachine = new VendingMachine(stocker.ReturnStock(path));
+
+			vendingMachine.DispenseItem("A3");
+			vendingMachine.DispenseItem("A3");
+			vendingMachine.DispenseItem("A3");
+			vendingMachine.DispenseItem("A3");
+			vendingMachine.DispenseItem("A3");
+			bool result = vendingMachine.ProductInStock(selection);
+
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
